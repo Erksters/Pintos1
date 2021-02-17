ba0b84bf96b3197ea602f957ad179a8f6c77c3a4
Erick PC <erksters@gmail.com>
2021-02-16 18:35:43 -0600

    kinux98 changes

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -23,6 +23,9 @@ typedef int tid_t;
 #define PRI_MIN 0                       /* Lowest priority. */
 #define PRI_DEFAULT 31                  /* Default priority. */
 #define PRI_MAX 63                      /* Highest priority. */
+#define NICE_MIN -20
+#define NICE_DEFAULT 0
+#define NICE_MAX 20
 
 /* A kernel thread or user process.
 
@@ -87,12 +90,14 @@ struct thread
     enum thread_status status;          /* Thread state. */
     char name[16];                      /* Name (for debugging purposes). */
     uint8_t *stack;                     /* Saved stack pointer. */
-    int priority;                       /* Priority. */
+    uint64_t priority;                  /* Priority. */
+    struct list donated_locks;
     struct list_elem allelem;           /* List element for all threads list. */
 
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
-
+    int nice;
+    int recent_cpu;
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
     uint32_t *pagedir;                  /* Page directory. */
@@ -106,6 +111,12 @@ struct thread
    If true, use multi-level feedback queue scheduler.
    Controlled by kernel command-line option "-o mlfqs". */
 extern bool thread_mlfqs;
+
+bool
+priority_desc (const struct list_elem *a_, const struct list_elem *b_,
+            void *aux UNUSED);
+
+uint64_t get_actual_priority(const struct thread *t);
 
 void thread_init (void);
 void thread_start (void);
@@ -137,5 +148,11 @@ int thread_get_nice (void);
 void thread_set_nice (int);
 int thread_get_recent_cpu (void);
 int thread_get_load_avg (void);
+
+void thread_mlfqs_increase_recent_cpu(void);
+void thread_mlfqs_set_load_avg(void);
+void thread_mlfqs_set_other(void);
+void calculate_recent_cpu(struct thread* t, void * x UNUSED);
+void thread_mlfqs_calculated_other_priority(void);
 
 #endif /* threads/thread.h */
